73d30d514993b8dfbc55519199c66d1373948d7a
Alessandro-Frisone <alefri2004@gmail.com>
2024-12-05 14:43:39 +0100

    feat: fourth ex

--- ex-js-functions-snack-main/01 - Iniziali/script.js
+++ ex-js-functions-snack-main/01 - Iniziali/script.js
@@ -1,5 +1,4 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
-
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
@@ -7,7 +6,6 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
