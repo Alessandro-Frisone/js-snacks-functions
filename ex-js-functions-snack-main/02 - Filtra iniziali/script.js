f5c93db16f448f6634b6014608c31d4eed721c06
Alessandro-Frisone <alefri2004@gmail.com>
2024-12-05 15:38:05 +0100

    feat: second ex

--- ex-js-functions-snack-main/02 - Filtra iniziali/script.js
+++ ex-js-functions-snack-main/02 - Filtra iniziali/script.js
@@ -2,12 +2,20 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
-
+function filterByLetter(namesArray, letter) {
+    const filteredArray = [];
+    for (let i = 0; i < namesArray.length; i++) {
+        if (namesArray[i][0] === letter) {
+            filteredArray.push(namesArray[i]);
+        }
+    }
+    return filteredArray;
+}
+const result = filterByLetter(names, "a"); 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(result);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
