fe197a1747e23f495e74d93f0fbe09bed77999a4
Alessandro-Frisone <alefri2004@gmail.com>
2024-12-05 15:19:50 +0100

    feat: third ex

--- ex-js-functions-snack-main/03 - conta vocali/script.js
+++ ex-js-functions-snack-main/03 - conta vocali/script.js
@@ -1,13 +1,19 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
+const word = 'parallelepipedo';
 
 
 // Dichiara la funzione qui.
-
-
+function countWord(word) {
+    let vocali = 0;
+    for (let i = 0; i < word.length; i++) {
+        if ('aeiou'.includes(word[i].toLowerCase())) {
+            vocali++;
+            }
+        }
+        return vocali;
+}
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(countWord(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
