deccecab3fb929a6d09c33b54f0f6827b96857c0
Alessandro-Frisone <alefri2004@gmail.com>
2024-12-05 16:38:34 +0100

    feat: first ex

--- ex-js-functions-snack-main/01 - Iniziali/script.js
+++ ex-js-functions-snack-main/01 - Iniziali/script.js
@@ -3,9 +3,15 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
-
+function firstLetter(namesArray) {
+    const result = [];
+    for (let i = 0; i < namesArray.length; i++) {
+      result.push(namesArray[i][0]);
+    }
+    return result;
+  }
+const finalFirstLetter = firstLetter(names);
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(finalFirstLetter);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
